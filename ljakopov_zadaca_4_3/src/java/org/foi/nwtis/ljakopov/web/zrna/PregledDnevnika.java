a5c640a1bad614178f547fc4bfdf9ea52059968c
Lovro <ljakopov@foi.hr>
2017-05-22 01:12:30 +0200

    Dodani session bean  i sređen html za ispis dnevnika

---
+++ ljakopov_zadaca_4_3/src/java/org/foi/nwtis/ljakopov/web/zrna/PregledDnevnika.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.nwtis.ljakopov.web.zrna;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.Date;
+import javax.ejb.EJB;
+import org.foi.nwtis.ljakopov.ejb.sb.DnevnikFacade;
+
+/**
+ *
+ * @author Lovro
+ */
+@Named(value = "pregledDnevnika")
+@SessionScoped
+public class PregledDnevnika implements Serializable {
+
+    @EJB
+    private DnevnikFacade dnevnikFacade;
+
+    private String korisnik;
+    private String url;
+    private String ipAdresa;
+    private Date vrijeme;
+    private int trajanje;
+    private int status;
+    
+    
+    
+    /**
+     * Creates a new instance of PregledDnevnika
+     */
+    public PregledDnevnika() {
+    }
+
+    public String getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(String korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIpAdresa() {
+        return ipAdresa;
+    }
+
+    public void setIpAdresa(String ipAdresa) {
+        this.ipAdresa = ipAdresa;
+    }
+
+    public Date getVrijeme() {
+        return vrijeme;
+    }
+
+    public void setVrijeme(Date vrijeme) {
+        this.vrijeme = vrijeme;
+    }
+
+    public int getTrajanje() {
+        return trajanje;
+    }
+
+    public void setTrajanje(int trajanje) {
+        this.trajanje = trajanje;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    
+}
